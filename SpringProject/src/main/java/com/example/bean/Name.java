c7c4d42a2a2bd0ad65980888ec7cbceffa5cf1b9
hp <hp@DESKTOP-R5DGJG4>
2020-04-10 13:58:38 -0700

    NameAddressProject

---
+++ SpringProject/src/main/java/com/example/bean/Name.java
@@ -0,0 +1,21 @@
+package com.example.bean;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
+public class Name {
+	private String FirstName;
+	private String LastName;
+	private Address address;
+	
+}

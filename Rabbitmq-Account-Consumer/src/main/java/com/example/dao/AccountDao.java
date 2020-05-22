e55b3d147476294bdb20abaee2aba4f9bcf300a5
14Priya41 <dhingrapriya636@gmail.com>
2020-05-21 21:02:03 -0700

    Spring Security in Eureka

--- Rabbitmq-Account-Consumer/src/main/java/com/example/dao/AccountDao.java
+++ Rabbitmq-Account-Consumer/src/main/java/com/example/dao/AccountDao.java
@@ -1,7 +1,9 @@
 package com.example.dao;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.model.Account;
+@Repository
 public interface AccountDao extends CrudRepository<Account, Integer>{
 
 }
---
+++ Task_Management_Eureka_Server/src/main/java/com/example/security/WebSecurity.java
@@ -0,0 +1,18 @@
+package com.example.security;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+    http
+            .csrf()
+            .disable()
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+}
